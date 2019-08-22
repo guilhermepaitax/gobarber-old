d01b92857187bcecd1fc89869b06bd9f880b6598
guilhermepaitax <[guilhermepaitax16@gmail.com]>
2019-08-22 15:50:46 -0300

    Middleware de Autenticação

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,17 @@
 import { Router } from 'express';
 
-import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import UserController from './app/controllers/UserController';
+
+import authMiddleare from './app/middlewares/auth';
 
 const routes = new Router();
 
-routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
+routes.post('/users', UserController.store);
+
+routes.use(authMiddleare);
+
+routes.put('/users', UserController.update);
 
 export default routes;
