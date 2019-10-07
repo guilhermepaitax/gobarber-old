3310dde22e2ed93dacf5d181589a077fcc6d1bfa
Guilherme Paitax <guilhermepaitax16@gmail.com>
2019-10-07 17:13:22 -0300

    Retornando informações do usuário

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -39,10 +39,10 @@ class SessionController {
       return res.status(401).json({ error: 'Password does not match' });
     }
 
-    const { id, name, avatar } = user;
+    const { id, name, avatar, provider } = user;
 
     return res.json({
-      user: { id, name, email, avatar },
+      user: { id, name, email, avatar, provider },
       token: jwt.sign({ id }, authConfig.secret, {
         expiresIn: authConfig.expiresIn,
       }),
